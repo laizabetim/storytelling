09e7515b182783cc710bfffa86953b7b52a0f985
laizabetim <141734112+laizabetim@users.noreply.github.com>
2025-06-16 12:25:29 -0300

    shein

--- missao-refletindo-sobre-inteligencia-artificial-aula-11/script.js
+++ missao-refletindo-sobre-inteligencia-artificial-aula-11/script.js
@@ -6,41 +6,41 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: "voce consegue lidar bem com o estresse do dia a dia?",
+        enunciado: "Voce consegue lidar bem com o estresse do dia a dia?",
         alternativas: [
             {
-                texto: "Isso é assustador!",
-                afirmacao: "No início ficou com medo do que essa tecnologia pode fazer. "
+                texto: "sim geralmente consigo me manter calmo e equilibrado.",
+                afirmacao: "Praticar respiracao profunda e manter uma rotina organizada tem me ajudado a lidar melhor com as pressoes diarias.",
             },
             {
-                texto: "Isso é maravilhoso!",
-                afirmacao: "Quis saber como usar IA no seu dia a dia."
+                texto: "As vezes, sinto que o estresse me sobrecarrega.",
+                afirmacao: "Tenho percebido que preciso encontrar maneiras mais saudaveis de relaxar e cuidar da saude mental.",
             }
         ]
     },
     {
-        enunciado: "Com a descoberta desta tecnologia, chamada Inteligência Artificial, uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre esta tecnologia. No fim de uma aula ela pede que você escreva um trabalho sobre o uso de IA em sala de aula. Qual atitude você toma?",
+        enunciado: "Voce costuma reservar um tempo para cuidar da sua saude emocional?",
         alternativas: [
             {
-                texto: "Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
-                afirmacao: "Conseguiu utilizar a IA para buscar informações úteis."
+                texto: "Sim, faco atividades que me ajudam a relaxar e me sentir bem.",
+                afirmacao: "Praticar exercicios fisicos e meditacao tem sido essencial para manter meu equilibrio emocional.",
             },
             {
-                texto: "Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema.",
-                afirmacao: "Sentiu mais facilidade em utilizar seus próprios recursos para escrever seu trabalho."
+                texto: "Ainda nao, mas quero comecas a dedicar mais tempo a isso.",
+                afirmacao: "Tenho percebido a importancia de cuidar da minha saude emocional e estou buscando criar uma nova rotina.",
             }
         ]
     },
     {
-        enunciado: "Após a elaboração do trabalho escrito, a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
+        enunciado: "Voce se sente confortavel para expressar emocoes e sentimentos?",
         alternativas: [
             {
-                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
-                afirmacao: "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA."
+                texto: "Sim, consigo falar abertamente sobre isso.",
+                afirmacao: "Conversar com pessoas de confianca me ajuda a aliviar emocoes e entender melhor meus sentimentos."
             },
             {
-                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
-                afirmacao: "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética."
+                texto: "As vezes sinto que tenho dificuldade em colocar meus sentimentos em palavras.",
+                afirmacao: "Estou aprendendo aos poucos a me expressar melhor e reconhecer minhas emocoes com mais clareza."
             }
         ]
     },
